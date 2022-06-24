8ba65eb66e80f0cfc13db6810da29852c4f21110
amal <amalghannoudi@gmail.com>
2022-06-24 10:27:58 +0100

    last version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+<<<<<<< Updated upstream
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent
+=======
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
+    
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
