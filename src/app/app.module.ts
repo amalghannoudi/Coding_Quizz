b1709d8d09139f996e7a811eb716d59d072028c3
amal <amalghannoudi@gmail.com>
2022-06-24 10:18:40 +0100

    last version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
-
-=======
->>>>>>> 6602d42d56445af3103d8074782e9ee7aadb8475
 
 @NgModule({
   declarations: [
